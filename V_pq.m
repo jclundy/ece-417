d3ab9294dcaec015f206646d201257523b325da7
LifeIsABeach <laua002@hotmail.com>
2018-04-22 20:00:58 -0400

    Delete V_pq.m

--- V_pq.m
+++
@@ -1,9 +0,0 @@
-function V_star = V_pq(p, q, rho, theta)
-R_pq_val = R_pq(p, q, rho);
-exp_val = exp(1i*q*theta);
-V = R_pq_val * exp_val;
-V_star = conj(V);
-if(isnan(V_star))
-    V_star = 0;
-end
-end
