e4b64060089110b93eaa4b16896c695094d01976
LifeIsABeach <laua002@hotmail.com>
2018-04-22 20:01:07 -0400

    Delete NLFilter.m

--- NLFilter.m
+++
@@ -1,7 +0,0 @@
-function newImage = NLFilter(image, weightMatrix)
-newImage = zeros(size(image));
-% figure(4)
-for i = 1:size(weightMatrix, 3)
-    newImage = newImage + double(image).*weightMatrix(:, :, i);
-%     imshow(newImage);
-end
